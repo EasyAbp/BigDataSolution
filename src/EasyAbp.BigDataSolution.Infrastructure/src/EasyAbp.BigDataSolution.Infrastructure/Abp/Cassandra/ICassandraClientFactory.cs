f878278f8b90a32517d6c1f204a5690394b2eeb3
real-zony <zony@myzony.com>
2021-08-17 22:37:34 +0800

    feat: Completed Cassandra Client.

--- src/EasyAbp.BigDataSolution.Infrastructure/src/EasyAbp.BigDataSolution.Infrastructure/Abp/Cassandra/ICassandraClientFactory.cs
+++ src/EasyAbp.BigDataSolution.Infrastructure/src/EasyAbp.BigDataSolution.Infrastructure/Abp/Cassandra/ICassandraClientFactory.cs
@@ -1,17 +1,38 @@
-﻿using Volo.Abp.DependencyInjection;
+﻿using System.Threading.Tasks;
+using Cassandra;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
 
 namespace EasyAbp.BigDataSolution.Infrastructure.Abp.Cassandra
 {
     public interface ICassandraClientFactory : ISingletonDependency
     {
-        ICassandraClient Create();
+        Task<ICassandraClient> CreateAsync();
     }
 
     public class CassandraClientFactory : ICassandraClientFactory
     {
-        public ICassandraClient Create()
+        private readonly IConnectionStringResolver _connectionStringResolver;
+
+        private const int AddressIndex = 0;
+        private const int KeySpaceIndex = 1;
+
+        public CassandraClientFactory(IConnectionStringResolver connectionStringResolver)
         {
-            throw new System.NotImplementedException();
+            _connectionStringResolver = connectionStringResolver;
+        }
+
+        public async Task<ICassandraClient> CreateAsync()
+        {
+            var connectionStrings = (await _connectionStringResolver.ResolveAsync()).Split(';');
+
+            var cluster = Cluster.Builder()
+                .AddContactPoint(connectionStrings[AddressIndex])
+                .Build();
+
+            var session = await cluster.ConnectAsync(connectionStrings[KeySpaceIndex]);
+
+            return new CassandraClient(session);
         }
     }
 }
